4cf656074787a232323a36385043bb2b818e407a
tenhyunik <tenorhyunik@gmail.com>
2024-06-18 11:35:41 +0900

    email operator 2

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -10,7 +10,7 @@ with DAG(
 ) as dag:
     send_email_task = EmailOperator(
         task_id='send_email_task',
-        to='tenorhyunik@gmail.com',
+        to='tenorhyunik@naver.com',
         subject='Airflow 성공메일',
         html_content='Airflow 작업이 완료되었습니다'
     )
